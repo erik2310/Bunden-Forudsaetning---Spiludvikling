522604037e840ffd46518b31ca802587bc3800d7
niel905g <niel905g@edu.easj.dk>
2018-01-26 16:36:36 +0100

    Erik, se det her...

---
+++ src/erikogniels/BasicGameApp.java
@@ -0,0 +1,22 @@
+package erikogniels;
+
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.entity.Entities;
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.input.Input;
+import com.almasb.fxgl.input.UserAction;
+import com.almasb.fxgl.settings.GameSettings;
+import javafx.scene.input.KeyCode;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+import java.util.Map;
+
+public class BasicGameApp extends GameApplication {
+    @Override
+    protected void initSettings(GameSettings settings) {}
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
