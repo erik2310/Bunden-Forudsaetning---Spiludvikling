de2c23210ae4616fe2678d7e16452f0a35461871
Erik Ruhmanis <erik2310@edu.easj.dk>
2018-02-06 03:12:12 +0100

    Har lavet en hel masse, vil prøve at skrive det hele ind i vores Wiki.

--- src/com/erikogniels/labyrintspil/EntityType.java
+++ src/com/erikogniels/labyrintspil/EntityType.java
@@ -2,5 +2,5 @@ package com.erikogniels.labyrintspil;
 
 // Her kan man lave EntityTypes til vores Entitys
 public enum EntityType {
-    PLAYER, WALL, SLUTWALLLVL1, SLUTWALLLVL2, COIN
+    PLAYER, WALL, SLUTWALLLVL1, SLUTWALLLVL2, SLUTWALLLVL3, COIN
 }
